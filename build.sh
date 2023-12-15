92bcf3aa2447484e55d154424b2eb672489aa24a
Lucas.Barbosa <highenterprise2016@gmail.com>
2023-12-15 16:56:23 -0300

    adding changes to git diff

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ for app in $apps; do
 echo "apps/$app"
 
   # Check if the app directory has any changes
-  if [ -n "$(git diff --name-only "apps/$app" | grep "apps/$app")" ]; then
+  if [ -n "$(git diff --name-only origin/main "apps/$app" | grep "apps/$app")" ]; then
 
     echo "Zipping $app"
     cd "apps/$app/" && zip -r "${app}_$(date +"%Y%m%d%H%M").zip" .
@@ -19,7 +19,7 @@ echo "apps/$app"
 
     aws s3 cp "${app}_$(date +"%Y%m%d%H%M").zip" "s3://s3-landing-dev-lucas/apps/${app}/${app}_$(date +"%Y%m%d%H%M").zip"
 
-    rm "${app}.zip"
+    rm "${app}_$(date +"%Y%m%d%H%M").zip"
 
     cd ../..
   fi
