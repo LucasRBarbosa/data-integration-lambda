b13e88249be171fe4b584d6b8eb0656f6bc20648
Lucas.Barbosa <highenterprise2016@gmail.com>
2023-12-15 17:10:37 -0300

    testing build script

--- build.sh
+++ build.sh
@@ -6,10 +6,17 @@ apps=$(find apps -type f -name "*.py" -exec dirname {} \; | sed 's,^apps/,,')
 
 for app in $apps; do
 
+current_ref=$(echo $GITHUB_REF | sed 's/refs\/heads\///')
+
+# Check if it's a pull request and get the associated branch
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
+  current_ref=$(jq -r .pull_request.base.ref "$GITHUB_EVENT_PATH")
+fi
+
 echo "apps/$app"
 
   # Check if the app directory has any changes
-  if [ -n "$(git diff --name-only origin/main "apps/$app" | grep "apps/$app")" ]; then
+  if [ -n "$(git diff --name-only current_ref "apps/$app" | grep "apps/$app")" ]; then
 
     echo "Zipping $app"
     cd "apps/$app/" && zip -r "${app}_$(date +"%Y%m%d%H%M").zip" .
