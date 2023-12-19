7f9fe847259f12d27f92c5e734ca911fa86a6253
Lucas.Barbosa <highenterprise2016@gmail.com>
2023-12-18 22:40:48 -0300

    [bug] feature changes for testing

--- apps/_demo-hello-world/index.py
+++ apps/_demo-hello-world/index.py
@@ -1,3 +1,8 @@
 def lambda_handler(event, context):
     print("Hello World")
     print(event["key2"])
+    print(event["key1"])
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    })
